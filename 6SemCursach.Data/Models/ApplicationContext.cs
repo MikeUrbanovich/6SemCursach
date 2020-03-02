c81d41d15483823be56adf759aded7af2ffdbf7b
Michael Urbanovich <mi.urbanovich@gmail.com>
2020-03-02 12:16:58 +0300

    ok

--- 6SemCursach.Data/Models/ApplicationContext.cs
+++ 6SemCursach.Data/Models/ApplicationContext.cs
@@ -4,21 +4,44 @@ namespace _6SemCursach.Data.Models
 {
     public class ApplicationContext: DbContext
     {
-        public DbSet<Roles> Roles { get; set; }
-        public DbSet<Students> Students { get; set; }
-        public DbSet<Teachers> Teachers { get; set; }
-        public DbSet<Users> Users { get; set; }
-        public DbSet<Courses> Courses { get; set; }
-        public DbSet<Semesters> Semesters { get; set; }
-        
-        public ApplicationContext()
-        {
-            Database.EnsureDeleted();
-            Database.EnsureCreated();
-        }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Semester> Semesters { get; set; }
+
+        //public ApplicationContext()
+        //{
+        //    Database.EnsureDeleted();
+        //    Database.EnsureCreated();
+        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //Teachers-Courses
+            modelBuilder.Entity<Teacher>()
+            .HasOne(t => t.Course)
+            .WithOne(c => c.Teacher)
+            .HasForeignKey<Course>(c => c.IDTeacher);
+            //Teachers-User
+            modelBuilder.Entity<User>()
+            .HasOne(u => u.Teacher)
+            .WithOne(t => t.User)
+            .HasForeignKey<Teacher>(t => t.IDUser);
+            //Students-User
+            modelBuilder.Entity<User>()
+            .HasOne(u => u.Student)
+            .WithOne(s => s.User)
+            .HasForeignKey<Student>(s => s.IDUser);
+            //User-Roles
+            modelBuilder.Entity<User>()
+            .HasOne(u => u.Student)
+            .WithOne(s => s.User)
+            .HasForeignKey<Student>(s => s.IDUser);
+
+
+
             modelBuilder.Entity<CoursStud>()
                 .HasKey(t => new
                 {
@@ -37,6 +60,11 @@ namespace _6SemCursach.Data.Models
                .WithMany(c => c.CoursStud)
                .HasForeignKey(cs => cs.CourseId);
         }
-        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=ZENBOOK-UX510UX;Initial Catalog=db6SemCursachBibd;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;");
+        }
+
     }
 }
--- 6SemCursach.Data/Models/CoursStud.cs
+++ 6SemCursach.Data/Models/CoursStud.cs
@@ -4,12 +4,12 @@ namespace _6SemCursach.Data.Models
     public class CoursStud
     {
         public int CourseId { get; set; }
-        public Courses Course { get; set; }
+        public Course Course { get; set; }
 
         public int StudentId { get; set; }
-        public Students Student { get; set; }
+        public Student Student { get; set; }
 
         public int SemesterId { get; set; }
-        public Semesters Semester { get; set; }
+        public Semester Semester { get; set; }
     }
 }
